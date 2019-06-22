356cd224307b6b6c0303cee0766fc4d4abc7dea4
kai-calis <kai-calis@hotmail.com>
2019-06-23 01:45:35 +0200

    Pretty much the finished game

---
+++ Ezekiel Game 2D/Assets/Scripts/esc_quit.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class esc_quit : MonoBehaviour
+{
+  
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+}
