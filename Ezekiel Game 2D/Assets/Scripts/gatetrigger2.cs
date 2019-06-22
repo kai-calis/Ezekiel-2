7613541881c029a52adf57e33f2305794a88c6c3
kai-calis <kai-calis@hotmail.com>
2019-06-22 15:26:36 +0200

    pretty much the finished game minus a start/end screen/ music and the animation run cycle for the player

--- Ezekiel Game 2D/Assets/Scripts/gatetrigger2.cs
+++ Ezekiel Game 2D/Assets/Scripts/gatetrigger2.cs
@@ -13,19 +13,7 @@ public class gatetrigger2 : MonoBehaviour
         gate2.transform.position += new Vector3(0, 4, 0);
     }
 
-    //public Collider2D Player;
-    //public Collider2D gate2;
-
-
-    // private void OnTriggerEnter2D(Collider2D collider)
-    //{
-    //   if (collider.gate2 && GameObject.Player = true)
-    // {
-
-    //    gate2.transform.position += new Vector3(0, 4, 0);
-    //}
-    // }
-
+   
 
 
 }
